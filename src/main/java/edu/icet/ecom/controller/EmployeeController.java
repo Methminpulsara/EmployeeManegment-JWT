5f18f121cb2022a84098005e9e7c00f56668cf00
Methminpulsara <methminpulsara10@gmail.com>
2025-05-02 23:51:05 +0530

    setup SecurityConfig

--- src/main/java/edu/icet/ecom/controller/EmployeeController.java
+++ src/main/java/edu/icet/ecom/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package edu.icet.ecom.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,10 @@ public class EmployeeController {
     @GetMapping
     public String getEmployee(){
         return "have";
+    }
+
+    @PostMapping("/login")
+    public String login(){
+        return "login";
     }
 }
