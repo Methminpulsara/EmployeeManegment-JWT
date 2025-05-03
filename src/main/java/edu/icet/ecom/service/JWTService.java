022f3d3c96b1b350a138218a8888b2300d7c03a1
Methminpulsara <methminpulsara10@gmail.com>
2025-05-03 22:44:19 +0530

    Create JWTService

---
+++ src/main/java/edu/icet/ecom/service/JWTService.java
@@ -0,0 +1,15 @@
+package edu.icet.ecom.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+    public String generateToken(String username){
+        //TODO: generate token
+        return "token";
+    }
+
+
+
+}
